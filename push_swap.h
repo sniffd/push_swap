73d9dc492fac018f39eb4cca0790fc014074e5e8
fdaryn-h <syndima43@yandex.ru>
2019-08-25 17:25:31 +0300

    added prototypes

--- push_swap.h
+++ push_swap.h
@@ -14,4 +14,15 @@ void	ft_qsort(int *ar, int end);
 int		atoi_push_swap(const char *str, int *f);
 void	add_last(t_stack **first, t_stack **last, t_stack *e);
 t_stack	*create_elem(int n);
+void	sa(t_stack **a_start);
+void	sb(t_stack **b_start);
+void	ss(t_stack **a_start, t_stack **b_start);
+void	ra(t_stack **a_start, t_stack **a_finish);
+void	rb(t_stack **b_start, t_stack **b_finish);
+void	rr(t_stack **a_start, t_stack **a_finish, t_stack **b_start, t_stack **b_finish);
+void	rra(t_stack **a_start, t_stack **a_finish);
+void	rrb(t_stack **b_start, t_stack **b_finish);
+void	rrr(t_stack **a_start, t_stack **a_finish, t_stack **b_start, t_stack **b_finish);
+void	pa(t_stack **a_start, t_stack **b_start);
+void	pb(t_stack **a_start, t_stack **b_start);
 #endif //PUSH_SWAP_PUSH_SWAP_H
