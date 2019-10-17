521af8c6e2e26568cbdcaefda942401b4bf5bc51
fdaryn-h <syndima43@yandex.ru>
2019-10-17 23:49:07 +0300

    fixed printing

--- r.c
+++ r.c
@@ -1,21 +1,24 @@
 #include "push_swap.h"
 
-void	ra(t_stack **a_start, t_stack **a_finish)
+void	ra(t_stack **a_start, t_stack **a_finish, int flag)
 {
 	*a_start = (*a_start)->next;
 	*a_finish = (*a_finish)->next;
-	ft_printf("ra\n");
+	if (flag)
+		ft_printf("ra\n");
 }
 
-void	rb(t_stack **b_start, t_stack **b_finish)
+void	rb(t_stack **b_start, t_stack **b_finish, int flag)
 {
 	*b_start = (*b_start)->next;
 	*b_finish = (*b_finish)->next;
-	ft_printf("rb\n");
+	if (flag)
+		ft_printf("rb\n");
 }
 
 void	rr(t_stack **a_start, t_stack **a_finish, t_stack **b_start, t_stack **b_finish)
 {
-	ra(a_start, a_finish);
-	rb(b_start, b_finish);
+	ra(a_start, a_finish, 0);
+	rb(b_start, b_finish, 0);
+	ft_printf("rr\n");
 }
--- rr.c
+++ rr.c
@@ -1,21 +1,24 @@
 #include "push_swap.h"
 
-void	rra(t_stack **a_start, t_stack **a_finish)
+void	rra(t_stack **a_start, t_stack **a_finish, int flag)
 {
 	*a_start = (*a_start)->prev;
 	*a_finish = (*a_start)->prev;
-	ft_printf("rra\n");
+	if (flag)
+		ft_printf("rra\n");
 }
 
-void	rrb(t_stack **b_start, t_stack **b_finish)
+void	rrb(t_stack **b_start, t_stack **b_finish, int flag)
 {
 	*b_start = (*b_start)->prev;
 	*b_finish = (*b_start)->prev;
-	ft_printf("rrb\n");
+	if (flag)
+		ft_printf("rrb\n");
 }
 
 void	rrr(t_stack **a_start, t_stack **a_finish, t_stack **b_start, t_stack **b_finish)
 {
-	rra(a_start, a_finish);
-	rrb(b_start, b_finish);
+	rra(a_start, a_finish, 0);
+	rrb(b_start, b_finish, 0);
+	ft_printf("rrr\n");
 }
