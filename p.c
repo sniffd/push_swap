d2cc6b46f087b67d00d312190e8bf4aaf99f272b
fdaryn-h <syndima43@yandex.ru>
2019-10-20 17:08:49 +0300

    refactored pa and pb; added one-element case

--- p.c
+++ p.c
@@ -1,61 +1,109 @@
 #include "push_swap.h"
 
-void	pa(t_pointers *pntrs)//TODO сделать обработку стека из одного элемента;
+void	pa_several(t_pointers *pntrs)
+{
+	if (pntrs->a_first)
+	{
+		pntrs->a_first->prev->next = pntrs->b_first;
+		(pntrs->b_first)->prev->next = (pntrs->b_first)->next;
+		(pntrs->b_first)->next->prev = (pntrs->b_first)->prev;
+		(pntrs->b_first)->prev = pntrs->a_first->prev;
+		pntrs->b_first = (pntrs->b_first)->next;
+		pntrs->a_first->prev->next->next = pntrs->a_first;
+		pntrs->a_first->prev = pntrs->a_first->prev->next;
+		pntrs->a_first = pntrs->a_first->prev;
+	}
+	else
+	{
+		pntrs->a_first = pntrs->b_first;
+		pntrs->b_first = (pntrs->b_first)->next;
+		pntrs->a_first->prev->next = pntrs->b_first;
+		(pntrs->b_first)->prev = pntrs->a_first->prev;
+		pntrs->a_first->prev = pntrs->a_first;
+		pntrs->a_first->next = pntrs->a_first;
+	}
+	pntrs->a_last = pntrs->a_first->prev;
+	pntrs->b_last = (pntrs->b_first) ? pntrs->b_first->prev : NULL;
+}
+
+void	pa(t_pointers *pntrs)
 {
 	if (pntrs->b_first)
 	{
-		if (pntrs->a_first)
+		if (pntrs->b_first == pntrs->b_last)
 		{
-			pntrs->a_first->prev->next = pntrs->b_first;
-			(pntrs->b_first)->prev->next = (pntrs->b_first)->next;
-			(pntrs->b_first)->next->prev = (pntrs->b_first)->prev;
-			(pntrs->b_first)->prev = pntrs->a_first->prev;
-			pntrs->b_first = (pntrs->b_first)->next;
-			pntrs->a_first->prev->next->next = pntrs->a_first;
-			pntrs->a_first->prev = pntrs->a_first->prev->next;
-			pntrs->a_first = pntrs->a_first->prev;
+			if (pntrs->a_first)
+			{
+				pntrs->a_last->next = pntrs->a_first;
+				pntrs->b_first->next = pntrs->a_first;
+				pntrs->a_first->prev = pntrs->b_first;
+				pntrs->b_first->prev = pntrs->a_last;
+				pntrs->a_first = pntrs->a_first->prev;
+			}
+			else
+			{
+				pntrs->a_first = pntrs->b_first;
+				pntrs->a_last = pntrs->b_last;
+			}
+			pntrs->b_first = NULL;
+			pntrs->b_last = NULL;
 		}
 		else
-		{
-			pntrs->a_first = pntrs->b_first;
-			pntrs->b_first = (pntrs->b_first)->next;
-			pntrs->a_first->prev->next = pntrs->b_first;
-			(pntrs->b_first)->prev = pntrs->a_first->prev;
-			pntrs->a_first->prev = pntrs->a_first;
-			pntrs->a_first->next = pntrs->a_first;
-		}
-		pntrs->a_last = pntrs->a_first->prev;
-		pntrs->b_last = (pntrs->b_first) ? pntrs->b_first->prev : NULL;
+			pa_several(pntrs);
 		ft_printf("pa\n");
 	}
+}
+
+void	pb_several(t_pointers *pntrs)
+{
+	if (pntrs->b_first)
+	{
+		pntrs->b_first->prev->next = pntrs->a_first;
+		pntrs->a_first->prev->next = pntrs->a_first->next;
+		pntrs->a_first->next->prev = pntrs->a_first->prev;
+		pntrs->a_first->prev = pntrs->b_first->prev;
+		pntrs->a_first = pntrs->a_first->next;
+		pntrs->b_first->prev->next->next = pntrs->b_first;
+		pntrs->b_first->prev = pntrs->b_first->prev->next;
+		pntrs->b_first = pntrs->b_first->prev;
+	}
+	else
+	{
+		pntrs->b_first = pntrs->a_first;
+		pntrs->a_first = pntrs->a_first->next;
+		pntrs->b_first->prev->next = pntrs->a_first;
+		pntrs->a_first->prev = pntrs->b_first->prev;
+		pntrs->b_first->prev = pntrs->b_first;
+		pntrs->b_first->next = pntrs->b_first;
+	}
+	pntrs->b_last = pntrs->b_first->prev;
+	pntrs->a_last = (pntrs->a_first) ? pntrs->a_first->prev : NULL;
 }
 
 void	pb(t_pointers *pntrs)
 {
 	if (pntrs->a_first)
 	{
-		if (pntrs->b_first)
+		if (pntrs->a_first == pntrs->a_last)
 		{
-			pntrs->b_first->prev->next = pntrs->a_first;
-			pntrs->a_first->prev->next = pntrs->a_first->next;
-			pntrs->a_first->next->prev = pntrs->a_first->prev;
-			pntrs->a_first->prev = pntrs->b_first->prev;
-			pntrs->a_first = pntrs->a_first->next;
-			pntrs->b_first->prev->next->next = pntrs->b_first;
-			pntrs->b_first->prev = pntrs->b_first->prev->next;
-			pntrs->b_first = pntrs->b_first->prev;
+			if (pntrs->b_first)
+			{
+				pntrs->b_last->next = pntrs->b_first;
+				pntrs->a_first->next = pntrs->b_first;
+				pntrs->b_first->prev = pntrs->a_first;
+				pntrs->a_first->prev = pntrs->b_last;
+				pntrs->b_first = pntrs->b_first->prev;
+			}
+			else
+			{
+				pntrs->b_first = pntrs->a_first;
+				pntrs->b_last = pntrs->a_last;
+			}
+			pntrs->a_first = NULL;
+			pntrs->a_last = NULL;
 		}
 		else
-		{
-			pntrs->b_first = pntrs->a_first;
-			pntrs->a_first = pntrs->a_first->next;
-			pntrs->b_first->prev->next = pntrs->a_first;
-			pntrs->a_first->prev = pntrs->b_first->prev;
-			pntrs->b_first->prev = pntrs->b_first;
-			pntrs->b_first->next = pntrs->b_first;
-		}
-		pntrs->b_last = pntrs->b_first->prev;
-		pntrs->a_last = (pntrs->a_first) ? pntrs->a_first->prev : NULL;
+			pb_several(pntrs);
 		ft_printf("pb\n");
 	}
 }
