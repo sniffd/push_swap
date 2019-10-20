df373b23d12f2b8f0ba53e54aa6c9ed4d6070092
fdaryn-h <syndima43@yandex.ru>
2019-10-20 18:59:43 +0300

    refactored sa sb and ss

--- s.c
+++ s.c
@@ -1,17 +1,23 @@
 #include "push_swap.h"
 
-void	sa(t_stack **a_start)
+void	sa(t_pointers *pntrs, int flag)
 {
-	ft_swap(&(*a_start)->data, &(*a_start)->next->data);
+	ft_swap(&(pntrs->a_first->data), &(pntrs->a_first->next->data));
+	if (flag)
+		ft_printf("sa\n");
 }
 
-void	sb(t_stack **b_start)
+void	sb(t_pointers *pntrs, int flag)
 {
-	ft_swap(&(*b_start)->data, &(*b_start)->next->data);
+	ft_swap(&(pntrs->b_first->data), &(pntrs->b_first->next->data));
+	if (flag)
+		ft_printf("sb\n");
 }
 
-void	ss(t_stack **a_start, t_stack **b_start)
+void	ss(t_pointers *pntrs, int flag)
 {
-	sa(a_start);
-	sb(b_start);
+	sa(pntrs, 0);
+	sb(pntrs, 0);
+	if (flag)
+		ft_printf("ss\n");
 }
